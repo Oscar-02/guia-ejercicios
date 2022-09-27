5534a4b8fb689792531dba5bea4ff6d278213c71
Oscar-02 <oscacanas29@gmail.com>
2022-09-26 21:23:39 -0600

    Base Pagina terminada

---
+++ index.js
@@ -0,0 +1,10 @@
+const mainPage = '/html/main.html';
+ej1 = '/html/1.html';
+
+function mainPageCall(){
+    document.getElementById('contentFrame').src = mainPage;
+}
+
+function ej1Call(){
+    document.getElementById('contentFrame').src = ej1;
+}
