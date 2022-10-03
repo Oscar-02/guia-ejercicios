b268589eb886e07c621502f3fc10053aab23c44c
Oscar-02 <oscacanas29@gmail.com>
2022-10-02 20:16:41 -0600

    EJ5 v1.1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mainPage = 'pages/main.html';
 ej1 = 'pages/1/1.html';
 ej2 = 'pages/2/2.html';
+ej3 = 'pages/5/5.html'
 
 function mainPageCall(){
     document.getElementById('contentFrame').src = mainPage;
@@ -12,4 +13,8 @@ function ej1Call(){
 
 function ej2Call(){
     document.getElementById('contentFrame').src = ej2;
+}
+
+function ej3Call(){
+    document.getElementById('contentFrame').src = ej5;
 }
