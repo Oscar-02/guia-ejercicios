698919a8f4dc1a893b6dbfd616130717c802e50c
Oscar-02 <oscacanas29@gmail.com>
2022-10-03 07:48:25 -0600

    EJ3 v1.0

--- pages/3/3.js
+++ pages/3/3.js
@@ -1,21 +1,41 @@
-var n = prompt("Ingrese la cantidad de alumnos", "");
-var n1, n2, n3;
-var i = 1;
-var j = 1;
-var prom;
+const nombre = document.getElementById("namePerson");
+const nota1 = document.getElementById("value1");
+const nota2 = document.getElementById("value2");
+const nota3 = document.getElementById("value3");
 
-const datos = [n, 3];
+var nombres = new Array();
+var notas = new Array();
 
-while(i <= n){
-    nombre = prompt("Ingrese el nombre del alumno")
-    alert(nombre);
-    n1 = parseFloat(prompt("Nota 1: "));
-    n2 = parseFloat(prompt("Nota 2: "));
-    n3 = parseFloat(prompt("Nota 3: "));
-    prom = Math.random((n1 + n2 + n3)/3)
-    alert("El promedio  del alumno es: " + prom);
-    i = i + 1;
+function onClickButton(){
+    var nombreVal = nombre.value.toString();
+    var nota1Val = parseFloat(nota1.value);
+    var nota2Val = parseFloat(nota2.value);
+    var nota3Val = parseFloat(nota3.value);
+
+    if (nombreVal && nota1Val && nota2Val && nota3Val){
+        if (nota1Val < 0 || nota2Val < 0 || nota3Val < 0){
+            alert('Lo sentimos, puede que algunos de los valores ingresados es menor a la nota minima (0.0).')
+        }
+        else if (nota1Val > 10 || nota2Val > 10 || nota3Val > 10){
+            alert('Lo sentimos, puede que algunos de los valores ingresados es mayor a la nota maxima (10.0).')
+        }
+        else{
+            var prom = (nota1Val + nota2Val + nota3Val) / 3;
+        var data = document.getElementById("info").getElementsByTagName("tbody")[0].insertRow();
+        var cell0 = data.insertCell(0);
+        var cell1 = data.insertCell(1);
+        var cell2 = data.insertCell(2);
+        var cell3 = data.insertCell(3);
+        var cell4 = data.insertCell(4);
+        cell0.innerHTML = nombreVal;
+        cell1.innerHTML = nota1Val.toFixed(1);
+        cell2.innerHTML = nota2Val.toFixed(1);
+        cell3.innerHTML = nota3Val.toFixed(1);
+        cell4.innerHTML = prom.toFixed(1);
+        }
+    }
+    else alert('Lo sentimos, pero uno o varios de los campos estan vacios o no tienen el formato correcto.');
 }
 
-
-
+//var data = document.getElementById("info").getElementsByTagName('tbody')[0].insertRow(); 
+// data.innerHTML = "cell";
